760457230ae9caba1be2fbc9001e4656d42f8667
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 22:35:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "source_bucketname" {
+  type        = string
   value = "source_bucket-hcl-us-east1-bucket"
 }
 
 
 variable "dest_bucketname" {
+  type        = string
   value = "dest_bucket-hcl-us-east1-bucket"
 }
