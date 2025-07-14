62e7bd7985e030b087d195951c0cebde976a2950
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 21:35:57 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,4 +2,9 @@ variables "source_bucketname" {
   type = string
   value = "source_bucket-hcl-us-east1-bucket"
 }
-  
+
+
+variables "dest_bucketname" {
+  type = string
+  value = "dest_bucket-hcl-us-east1-bucket"
+}
