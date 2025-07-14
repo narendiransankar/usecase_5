243de11261113eae2e79cc2181ce95ba0f135388
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 22:32:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   backend "s3" {
     bucket = "hcl-test-backend-state"
     key    = "usecase5/terraform.tfstate"
-    region = "us-east-1"
-    #use_lockfile   = true
+    region = "ap-south-1"
   }
 }
