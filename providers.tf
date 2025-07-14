f56b663e60f4aeb3ea837e52ddeaa8d5f87359c1
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 15:04:20 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.95.0, < 6.0.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
