8fea91c0f69e48c303605244e7f7f17e092ecf56
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 22:50:21 +0530

    Create variables.tf

---
+++ modules/lambda/variables.tf
@@ -0,0 +1,7 @@
+variable "lambda_function_name" {}
+variable "handler_name" {}
+variable "runtime" {}
+variable "source_bucket_name" {}
+variable "destination_bucket_name" {}
+variable "sns_topic_arn" {}
+variable "lambda_s3_trigger_arn" {}
