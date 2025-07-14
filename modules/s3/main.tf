ea05bc9a9b5191faee59f2c29190092e7e9173ce
narendiransankar <narendiransankar1699@gmail.com>
2025-07-14 21:35:20 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "source_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "dest_bucket" {
+  bucket = var.dest_bucketname
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
