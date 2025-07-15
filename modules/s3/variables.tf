1be095637811583f6cfd2c14941bd9660949d7d1
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 08:57:57 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,3 +1,7 @@
 variable "source_bucket_name" {}
 variable "destination_bucket_name" {}
 variable "lambda_function_arn" {}
+variable "lambda_permission_dependency" {
+  type = any
+  default = null
+}
