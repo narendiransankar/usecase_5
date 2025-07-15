c655fac257a3d95eba968e843852bd77d9dbf8bc
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 07:07:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "lambda" {
   source                  = "./modules/lambda"
   lambda_function_name    = var.lambda_function_name
   handler_name            = "image_processor.lambda_handler"
-  runtime                 = "python3.11"
+  runtime                 = "python3.9"
   source_bucket_name      = var.source_bucket_name
   destination_bucket_name = var.destination_bucket_name
   sns_topic_arn           = module.sns.topic_arn
