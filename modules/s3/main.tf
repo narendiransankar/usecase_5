0b32341cb9fac4922c727e8952a9935d330bceec
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 08:52:08 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,5 +14,5 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
     lambda_function_arn = var.lambda_function_arn
   }
 
-  depends_on = [aws_s3_bucket.source]
+  depends_on = [aws_s3_bucket.source,aws_lambda_permission.allow_s3]
 }
