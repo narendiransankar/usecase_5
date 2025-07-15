093668f7c1f5b3d20abd9f1f03d1df751441515b
narendiransankar <narendiransankar1699@gmail.com>
2025-07-15 06:54:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,18 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
-terraform {
-  required_providers {
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.2"
     }
   }
+}
+
+provider "aws" {
+  region = var.region
 }
